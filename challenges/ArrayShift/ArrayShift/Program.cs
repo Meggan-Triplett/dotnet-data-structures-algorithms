b5aa60fe53c5ade07cfaa28418f2659949277ca9
Meggan <meggtriplett@gmail.com>
2019-06-19 11:35:46 -0700

    Added arrayshift project

---
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //insertShiftArray();
+        }
+
+        static int[] insertShiftArray(int[] inputArray, int value)
+        {
+            int[] newArray = new int[inputArray.Length + 1];
+
+            double middleIndex = inputArray.Length - 1 / 2;
+
+            return inputArray;
+        }
+    }
+}

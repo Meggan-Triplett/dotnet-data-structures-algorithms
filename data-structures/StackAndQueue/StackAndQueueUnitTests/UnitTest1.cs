492207848d308acc97e6c91600048fcafadbf08d
Meggan <meggtriplett@gmail.com>
2019-06-24 13:22:42 -0700

    Created unit test project and added to do tests. Completed CanInstatiateEmptyStack test

---
+++ data-structures/StackAndQueue/StackAndQueueUnitTests/UnitTest1.cs
@@ -0,0 +1,87 @@
+using System;
+using Xunit;
+using StackAndQueue.Classes;
+
+namespace StackAndQueueUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanPushOntoStack()
+        {
+            
+
+
+
+        }
+
+        [Fact]
+        public void CanPushMultipleValuesOntoStack()
+        {
+
+        }
+
+        [Fact]
+        public void CanPopOffStack()
+        {
+
+        }
+
+        [Fact]
+        public void CanEmptyStackAfterMultiplePops()
+        {
+
+        }
+
+        [Fact]
+        public void CanPeekNextItemOnStack()
+        {
+
+        }
+
+        [Fact]
+        public void CanInstantiateEmptyStack()
+        {
+            Stack stack = new Stack();
+
+            Assert.Null(stack.Top);
+        }
+
+        [Fact]
+        public void CanEnqueueIntoQueue()
+        {
+
+        }
+
+        [Fact]
+        public void CanEnqueueMultipleValuesIntoQueue()
+        {
+
+        }
+
+        [Fact]
+        public void CanDequeueOutOfQueueExpectedValue()
+        {
+
+        }
+
+        [Fact]
+        public void CanPeekIntoQueueExpectedValue()
+        {
+
+        }
+
+        [Fact]
+        public void CanEmptyQueueAfterMultipleDequeues()
+        {
+
+        }
+
+        [Fact]
+        public void CanInstantiateEmptyQueue()
+        {
+
+        }
+
+    }
+}

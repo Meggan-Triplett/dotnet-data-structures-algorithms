ffa3f2f51ef33c79d8bfdfac96ef7cc90779cbaf
Meggan <meggtriplett@gmail.com>
2019-06-28 00:33:17 -0700

    Creating a console app to generate pictures for each method

--- data-structures/Linked-List/Linked-List/Program.cs
+++ data-structures/Linked-List/Linked-List/Program.cs
@@ -21,6 +21,64 @@ namespace LinkedList
             list.Insert(1);
         }
 
+        static void UserInterface()
+        {
+            Console.WriteLine("Please choose from one of the following options:");
+            Console.WriteLine("1) Insert: Add New Node Into Existing Linked List");
+            Console.WriteLine("2) Includes: Check If Given Value Exists in Linked List");
+            Console.WriteLine("3) Print: Display Linked List");
+            Console.WriteLine("4) Exit");
+
+            string userSelection = Console.ReadLine();
+            int actionSelection = Convert.ToInt32(userSelection);
+
+            switch (actionSelection)
+            {
+                case 1:
+                    ViewBalance();
+                    ReturnToMenu();
+                    break;
+                case 2:
+                    WithdrawMoney(WithdrawRequest());
+                    ReturnToMenu();
+                    break;
+                case 3:
+                    DepositMoney(DepositRequest());
+                    ReturnToMenu();
+                    break;
+                case 4:
+                    Exit();
+                    break;
+                default:
+                    Exit();
+                    break;
+            }
+        }
+        static void ReturnToMenu()
+        {
+            Console.WriteLine("Would you like to return to menu? (Y/N)");
+
+            string userResponse = Console.ReadLine();
+            string userDecision = userResponse.ToUpper();
+
+            if (userDecision == "Y")
+            {
+                UserInterface();
+            }
+            else if (userDecision == "N")
+            {
+                Exit();
+            }
+            else
+            {
+                UserInterface();
+            }
+        }
+
+        static void Exit()
+        {
+            Environment.Exit(0);
+        }
 
     }
 }

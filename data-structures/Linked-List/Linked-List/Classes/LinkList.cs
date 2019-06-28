e19322f24c340c8e2207ba47357cafe7f7a0a55d
Meggan <meggtriplett@gmail.com>
2019-06-27 23:34:35 -0700

    Initial copy over of what I had in my other LinkedList project and repo

---
+++ data-structures/Linked-List/Linked-List/Classes/LinkList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Classes
+{
+    public class LinkList
+    {
+        public Node Head { get; set; }
+        public LinkList Current { get; set; }
+
+        /// <summary>
+        /// Creates a link list with one node which is set to head
+        /// </summary>
+        /// <param name="value">Value of first node to be created for link list</param>
+        public LinkList(int value)
+        {
+            Node node = new Node(value);
+            Head = node;
+        }
+
+        /// <summary>
+        /// Creates new node and puts it at the front of the link list
+        /// </summary>
+        /// <param name="value"> Value stored in the new node </param>
+        public void Insert(int value)
+        {
+            Node node = new Node(value);
+            node.Next = Head;
+            Head = node;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Includes(int value)
+        {
+            Node current = Head;
+
+            while (current != null)
+            {
+                if (current.Value == value)
+                {
+                    return true;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Display in console the value of each node in linked list
+        /// </summary>
+        public void Print()
+        {
+            Node current = Head;
+
+            while (current != null)
+            {
+                Console.Write($"{current.Value} -> ");
+                current = current.Next;
+            }
+
+            Console.WriteLine("null");
+        }
+    }
+
+}
---
+++ data-structures/Linked-List/Linked-List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LinkedList.Classes;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ConsoleLinkedList();
+        }
+
+        static void ConsoleLinkedList()
+        {
+            LinkList list = new LinkList(9);
+
+            list.Insert(4);
+            list.Insert(9);
+            list.Insert(1);
+
+            list.Print();
+        }
+
+
+    }
+}

2d57674e886e632136dae4a8fad138db8a102594
Meggan <meggtriplett@gmail.com>
2019-07-01 10:15:19 -0700

    Added binary tree class with property of Root

---
+++ data-structures/Tree/Tree/Classes/BinaryTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public BinaryTree()
+        {
+
+        }
+
+
+    }
+}

99505aa98bc163a8210b98330df59d458a575ff9
Meggan Triplett <meggtriplett@gmail.com>
2019-08-04 04:07:02 -0700

    Completed AddVertex method

--- data-structures/Graphs/Graphs/Classes/Graph.cs
+++ data-structures/Graphs/Graphs/Classes/Graph.cs
@@ -7,15 +7,22 @@ namespace Graphs.Classes
 {
     class Graph<T>
     {
-
+        //Collection of key value pairs of Vertices and their Edges
         public Dictionary<Vertex<T>, List<Edge<T>>> AdjacencyList { get; set; } 
 
+        //Graph constructor that is built from an AdjacencyList
         public Graph()
         {
             AdjacencyList = new Dictionary<Vertex<T>, List<Edge<T>>>();
         }
 
-        
+        public Vertex<T> AddVertex(T value)
+        {
+            Vertex<T> vertex = new Vertex<T>(value);
+            AdjacencyList.Add(vertex, new List<Edge<T>>());
+
+            return vertex;
+        }
 
     }
 }

bb6d1b098e138af5e462e13c300ba2d19cca2eee
Meggan Triplett <meggtriplett@gmail.com>
2019-08-04 05:40:16 -0700

    Added skeleton of unit tests

---
+++ data-structures/Graphs/XUnitTestGraphs/UnitTest1.cs
@@ -0,0 +1,51 @@
+using System;
+using Xunit;
+
+namespace XUnitTestGraphs
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void VertexCanBeAddedToGraph()
+        {
+
+        }
+
+        [Fact]
+        public void EdgeCanBeAddedToGraph()
+        {
+
+        }
+
+        [Fact]
+        public void AllVerticesCanBeRetrieved()
+        {
+
+        }
+
+        [Fact]
+        public void AllNeighborsOfGivenVertexCanBeRetrieved()
+        {
+
+        }
+
+        [Fact]
+        public void NumberOfNodesInGraphCanBeRetrieved()
+        {
+
+        }
+
+        [Fact]
+        public void GraphOfOneVertexAndEdgeCanBeReturned()
+        {
+
+        }
+
+        [Fact]
+        public void EmptyGraphReturnsNull()
+        {
+
+        }
+
+    }
+}
